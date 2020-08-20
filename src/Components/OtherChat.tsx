c825ea182b3b870b3fe6d9e7aaf1ac773612baa2
Mingyu-Song <sv002@naver.com>
2020-08-20 23:53:59 +0900

    [Add] react-transition-group 라이브러리 설치

--- src/Components/OtherChat.tsx
+++ src/Components/OtherChat.tsx
@@ -1,36 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
-import msgBefore from 'assets/msg_before_w.png';
-import msgAfter from 'assets/msg_after_w.png';
-import msgBeforeMain from 'assets/msg_before.png';
-import msgAfterMain from 'assets/msg_after.png';
+import React from "react";
+import styled from "styled-components";
+import msgBefore from "assets/msg_before_w.png";
+import msgAfter from "assets/msg_after_w.png";
+import msgBeforeMain from "assets/msg_before.png";
+import msgAfterMain from "assets/msg_after.png";
 
 type chatProps = {
-	message: string; // props로 받아서 출력할 message
-	isMain?: boolean; // main page에서 사용할경우 props로 true값을 넘겨주면 알아서 스타일 변경
-	img?: string; // 결과창에서 사용할 image
+  message: string; // props로 받아서 출력할 message
+  isMain?: boolean; // main page에서 사용할경우 props로 true값을 넘겨주면 알아서 스타일 변경
+  img?: string; // 결과창에서 사용할 image
 };
 
 const OtherChat = ({ message, isMain, img }: chatProps) => {
-	return (
-		<MessageBox>
-			<img alt='msg_before' src={isMain ? msgBeforeMain : msgBefore} />
-			<p className={isMain ? 'mainChatText' : 'otherChatText'}>{message}</p>
-			<img alt='msg_after' src={isMain ? msgAfterMain : msgAfter} />
-		</MessageBox>
-	);
+  return (
+    <MessageBox isMain={isMain}>
+      <img alt="msg_before" src={isMain ? msgBeforeMain : msgBefore} />
+      <p>{message}</p>
+      <img alt="msg_after" src={isMain ? msgAfterMain : msgAfter} />
+    </MessageBox>
+  );
 };
 
 export default OtherChat;
 
 const MessageBox = styled.article`
-	.otherChatText {
-		background-color: white;
-	}
+  background-color: ${(props) =>
+    props.isMain ? props.theme.lightGray : "white"};
 
-	.mainChatText {
-		background-color: ${(props) => props.theme.lightGray};
-	}
-
-	${(props) => props.theme.MessageBox}
+  ${(props) => props.theme.MessageBox}
 `;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from 'Styles/GlobalStyle';
-import theme from './Styles/theme';
-import Splash from 'Pages/Splash';
-import Test from 'Pages/TestForm';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "Styles/GlobalStyle";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+import theme from "Styles/theme";
+import Splash from "Pages/Splash";
+import Test from "Pages/TestForm";
 
 const Routes = () => {
-	return (
-		<Router>
-			<GlobalStyle />
-			<ThemeProvider theme={theme}>
-				<Switch>
-					<Route exact path='/splash' component={Splash} />
-					<Route exact path='/test' component={Test} />
-					<Redirect from='*' to='/' />
-				</Switch>
-			</ThemeProvider>
-		</Router>
-	);
+  return (
+    <Router>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/splash" component={Splash} />
+          <Route exact path="/test" component={Test} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </ThemeProvider>
+    </Router>
+  );
 };
 
 export default Routes;
