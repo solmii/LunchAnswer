47a7ab7550d2232d6358d863b0e4befea884b52c
Mingyu-Song <sv002@naver.com>
2020-08-27 00:46:29 +0900

    [fix] fragment태그 React.fragment 로 수정후 map함수 key값  부여

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,60 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 type ButtonProps = {
-	color: string;
-	children: React.ReactChild;
-	mouseAction?: boolean;
-	eventFunc: any;
+  color: string;
+  children: React.ReactChild;
+  mouseAction?: boolean;
+  eventFunc: any;
 };
 
 const Button = ({ color, mouseAction, children, eventFunc }: ButtonProps) => {
-	return (
-		<ButtonBox color={color} mouseAction={mouseAction} onClick={eventFunc}>
-			{children}
-		</ButtonBox>
-	);
+  return (
+    <ButtonBox color={color} mouseAction={mouseAction} onClick={eventFunc}>
+      {children}
+    </ButtonBox>
+  );
 };
 
 export default React.memo(Button);
 
 const ButtonBox = styled.button`
-	outline: none;
-	border: none;
-	width: 100%;
-	padding: 0.813em 0;
-	border-radius: 0.5em;
-	cursor: ${(props) => (props.mouseAction ? 'pointer' : 'not-allowed')};
-	pointer-events: ${(props) => (props.mouseAction ? 'auto' : 'none')};
+  outline: none;
+  border: none;
+  width: 100%;
+  padding: 0.813em 0;
+  border-radius: 0.5em;
+  cursor: ${(props) => (props.mouseAction ? "pointer" : "not-allowed")};
+  pointer-events: ${(props) => (props.mouseAction ? "auto" : "none")};
 
-	* {
-		font-size: 21.5px;
-		font-family: ${({ theme }) => theme.mainFontRegular};
-	}
+  * {
+    font-size: 21.5px;
+    font-family: ${({ theme }) => theme.mainFontRegular};
+  }
 
-	b {
-		font-family: ${({ theme }) => theme.mainFontBold};
-	}
+  b {
+    font-family: ${({ theme }) => theme.mainFontBold};
+  }
 
-	${({ color, theme }) => handleColorType(color, theme)}; /* 컬러 체킹 */
+  ${({ color, theme }) => handleColorType(color, theme)}; /* 컬러 체킹 */
 
-	&:active {
-		transform: translateY(7px);
-		box-shadow: 0px 0px;
-	}
+  &:active {
+    transform: translateY(7px);
+    box-shadow: 0px 0px;
+  }
 `;
 
 const handleColorType = (color, theme) => {
-	switch (color) {
-		case 'yellow':
-			return `background: ${theme.mainYellow}; box-shadow: 0px 7px ${theme.btnShadowYellow};`;
-		case 'white':
-			return `background: white; box-shadow: 0px 7px ${theme.btnShadowGray};`;
-		case 'gray':
-			return `background: ${theme.lightGray}; box-shadow: 0px 7px ${theme.btnShadowGray};`;
-		case 'gray-click':
-			return `background: ${theme.lightGray}; box-shadow: 0px 7px ${theme.btnShadowGray};
+  switch (color) {
+    case "yellow":
+      return `background: ${theme.mainYellow}; box-shadow: 0px 7px ${theme.btnShadowYellow};`;
+    case "white":
+      return `background: white; box-shadow: 0px 7px ${theme.btnShadowGray};`;
+    case "gray":
+      return `background: ${theme.lightGray}; box-shadow: 0px 7px ${theme.btnShadowGray};`;
+    case "gray-click":
+      return `background: ${theme.lightGray}; box-shadow: 0px 7px ${theme.btnShadowGray};
 								&:active{ color: white; background-color: ${theme.mainOrange}; }
 				`;
-	}
+  }
 };
--- src/Components/OtherChat.tsx
+++ src/Components/OtherChat.tsx
@@ -1,36 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
-import msgBefore from 'assets/msg_before_w.png';
-import msgAfter from 'assets/msg_after_w.png';
-import msgBeforeMain from 'assets/msg_before.png';
-import msgAfterMain from 'assets/msg_after.png';
-import ChatKeyFrames from 'Styles/ChatKeyFrames';
+import React from "react";
+import styled from "styled-components";
+import msgBefore from "assets/msg_before_w.png";
+import msgAfter from "assets/msg_after_w.png";
+import msgBeforeMain from "assets/msg_before.png";
+import msgAfterMain from "assets/msg_after.png";
+import ChatKeyFrames from "Styles/ChatKeyFrames";
 
 type chatProps = {
-	// children: React.ReactChild; // props로 받아서 출력할 message
-	children: React.ReactChild | React.ReactFragment | React.ReactPortal | boolean | null | undefined; // props로 받아서 출력할 message
-	isMain?: boolean; // main page에서 사용할경우 props로 true값을 넘겨주면 알아서 스타일 변경
-	isDelay?: string; // test page에서 채팅창 애니메이션 딜레이
+  // children: React.ReactChild; // props로 받아서 출력할 message
+  children:
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | boolean
+    | null
+    | undefined; // props로 받아서 출력할 message
+  isMain?: boolean; // main page에서 사용할경우 props로 true값을 넘겨주면 알아서 스타일 변경
+  isDelay?: string; // test page에서 채팅창 애니메이션 딜레이
 };
 
 const OtherChat = ({ children, isMain, isDelay }: chatProps) => {
-	return (
-		<MessageBox isMain={isMain} isDelay={isDelay}>
-			{children && <img alt='msg_before' src={isMain ? msgBeforeMain : msgBefore} />}
-			<p>{children}</p>
-			{children && <img alt='msg_after' src={isMain ? msgAfterMain : msgAfter} />}
-		</MessageBox>
-	);
+  return (
+    <MessageBox isMain={isMain} isDelay={isDelay}>
+      {children && (
+        <img alt="msg_before" src={isMain ? msgBeforeMain : msgBefore} />
+      )}
+      <p>{children}</p>
+      {children && (
+        <img alt="msg_after" src={isMain ? msgAfterMain : msgAfter} />
+      )}
+    </MessageBox>
+  );
 };
 
 export default React.memo(OtherChat);
 
 const MessageBox = styled.article`
-	p {
-		background-color: ${(props) => (props.isMain ? props.theme.lightGray : 'white')};
-	}
+  p {
+    background-color: ${(props) =>
+      props.isMain ? props.theme.lightGray : "white"};
+  }
 
-	animation: ${ChatKeyFrames} 0.8s linear 0s;
-	animation-delay: ${(props) => props.isDelay};
-	${(props) => props.theme.MessageBox};
+  animation: ${ChatKeyFrames} 0.8s linear 0s;
+  animation-delay: ${(props) => props.isDelay};
+  ${(props) => props.theme.MessageBox};
 `;
